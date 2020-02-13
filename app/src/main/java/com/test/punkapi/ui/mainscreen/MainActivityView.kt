25137c3c626bc8b62bf942b3a1544b448ef8299d
Pirata Ram <israis007@outlook.com>
2020-02-13 02:43:18 -0600

    last changes

--- app/src/main/java/com/test/punkapi/ui/mainscreen/MainActivityView.kt
+++ app/src/main/java/com/test/punkapi/ui/mainscreen/MainActivityView.kt
@@ -2,13 +2,14 @@ package com.test.punkapi.ui.mainscreen
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AbsListView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelStoreOwner
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.test.punkapi.App
 import com.test.punkapi.R
 import com.test.punkapi.databinding.ActivityMainBinding
@@ -21,6 +22,13 @@ class MainActivityView : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var mainVM: MainActivityViewModel
     private val TAG = "MainActivityView"
+    private var isScrolling = false
+    private var currentItems = 0
+    private var totalItems = 0
+    private var scrollOutItems = 0
+    private var beforePage = 6
+    private val pageStart = 6
+    private var lastpage = 6
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,24 +38,75 @@ class MainActivityView : AppCompatActivity() {
         binding.mainActivityViewModel = mainVM
         mainVM.activityViewModel = this
         binding.lifecycleOwner = this
-        mainVM.page.value = 1
-        mainVM.elementsByPage.value = App.instance.resources.getInteger(R.integer.elementsByPage)
-        mainVM.beers.observe(this@MainActivityView, Observer {
-            loadRecyclerView(it)
+        main_swiperl.setOnRefreshListener {
+            val page = mainVM.page.value!!.toInt()
+            if (page in 2..pageStart){
+                refreshData(true)
+            } else
+                main_swiperl.isRefreshing = false
+        }
+        val linearlm = LinearLayoutManager(this)
+        linearlm.orientation = RecyclerView.VERTICAL
+        main_recview.layoutManager = linearlm
+        main_recview.addOnScrollListener(object: RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                currentItems = linearlm.childCount
+                totalItems = linearlm.itemCount
+                scrollOutItems = linearlm.findFirstVisibleItemPosition()
+
+                if (isScrolling && (currentItems + scrollOutItems == totalItems)){
+                    isScrolling = false
+                    refreshData(false)
+                }
+            }
+
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL)
+                    isScrolling = true
+            }
         })
-//        mainVM.setup()
+        mainVM.page.value = pageStart
+        mainVM.elementsByPage.value = App.instance.resources.getInteger(R.integer.elementsByPage)
+        refreshData(false)
     }
 
-    private fun loadRecyclerView(beers: List<BeerModel>){
-        main_recview.layoutManager = GridLayoutManager(this@MainActivityView, 1)
+    private fun refreshData(isUp: Boolean){
+        if (isUp){
+            beforePage--
+            mainVM.page.value = beforePage
+        } else {
+            mainVM.page.value = lastpage
+            lastpage++
+        }
+        mainVM.beersList.removeObservers(this)
+        mainVM.busy.value = View.VISIBLE as Integer
+        val temp = mainVM.updateBeers(isUp)
+        mainVM.beersList.observe(this, Observer {
+            loadRecyclerView(it, temp, isUp)
+            main_swiperl.isRefreshing = false
+        })
+    }
+
+    private fun loadRecyclerView(beers: ArrayList<BeerModel>, itemsAdded: Int, isUp: Boolean){
         main_recview.adapter = beerAdapter(this, beers, object: beerAdapter.onBeerTouchEvent{
             override fun onTouchItem(beerModel: BeerModel) {
                 //Update ui Main
-                Log.d("Main", beerModel.toString())
+                Log.d("Main", "La chela seleccionada es ($beerModel.name):\n$beerModel")
             }
         })
         main_recview.setHasFixedSize(false)
         main_recview.isNestedScrollingEnabled = true
-        main_recview.adapter!!.notifyDataSetChanged()
+        var resta = beers.size - itemsAdded
+        repeat(itemsAdded) {
+            if (isUp){
+                main_recview.adapter!!.notifyItemInserted(it)
+            } else {
+                main_recview.adapter!!.notifyItemInserted(resta)
+                resta++
+            }
+        }
+        mainVM.busy.value = View.GONE as Integer
     }
 }
--- app/src/main/java/com/test/punkapi/ui/mainscreen/MainActivityViewModel.kt
+++ app/src/main/java/com/test/punkapi/ui/mainscreen/MainActivityViewModel.kt
@@ -1,9 +1,11 @@
 package com.test.punkapi.ui.mainscreen
 
-import androidx.annotation.Keep
-import androidx.lifecycle.*
-import com.test.punkapi.ui.base.BaseModel
+import android.view.View
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModelStoreOwner
+import androidx.lifecycle.viewModelScope
 import com.test.punkapi.rest.punkapi.RepositoryPunkAPI
+import com.test.punkapi.ui.base.BaseModel
 import com.test.punkapi.ui.mainscreen.models.BeerModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -13,19 +15,44 @@ class MainActivityViewModel: BaseModel() {
     val page = MutableLiveData<Int>()
     val elementsByPage = MutableLiveData<Int>()
     lateinit var activityViewModel: ViewModelStoreOwner
-//    val beers = MutableLiveData<List<BeerModel>>()
-//
-//    fun setup(){
-//        viewModelScope.launch(Dispatchers.IO) {
-//            beers.postValue(RepositoryPunkAPI.getBeers(activityViewModel, page.value!!.toInt(), elementsByPage.value!!.toInt()))
-//        }
-//    }
+    val beersList = MutableLiveData<ArrayList<BeerModel>>()
+    lateinit var beers: List<BeerModel>
+    private var temp = 0
+    var busy: MutableLiveData<Integer> = MutableLiveData()
 
-    @Keep
-    val beers: LiveData<List<BeerModel>> = liveData(Dispatchers.IO){
-        val retrievedData = RepositoryPunkAPI.getBeers(activityViewModel, page.value!!.toInt(), elementsByPage.value!!.toInt())
-        emit(retrievedData)
+    init {
+        busy.value = View.GONE as Integer
     }
+
+    fun getBussy() = busy
+
+    fun netxPage(pageNext: Int){
+        var pa = page.value!!.toInt()
+        if (pageNext > 1)
+            pa+= pageNext
+        page.value = pa
+    }
+
+    fun updateBeers(isUp: Boolean): Int {
+        temp = 0
+        if (beersList.value == null)
+            beersList.value = ArrayList()
+
+        viewModelScope.launch(Dispatchers.IO) {
+            beers = RepositoryPunkAPI.getBeers(activityViewModel, page.value!!.toInt(), elementsByPage.value!!.toInt())
+            beers.forEach { element ->
+                run {
+                    if (isUp)
+                        beersList.value!!.add(temp, element)
+                    else
+                        beersList.value!!.add(element)
+                    temp++
+                }
+            }
+        }
+        return temp
+    }
+
 
 
 }
